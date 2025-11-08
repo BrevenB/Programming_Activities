d3518dd510ea722de6c32ce299f47a3285d03489
Breven Blanchard <a02265483@usu.edu>
2025-11-08 15:40:27 -0700

    Add favorite colors list and address input validation
    Create a list of favorite colors and ask for user address input. Validate the address by replacing non-alphanumeric characters.

--- pa_week11.py
+++ pa_week11.py
@@ -1,1 +1,21 @@
+# 1.) Create and Display a list of your 3 favorite colors
+colors = ["Red", "White", "Blue"]
 
+fav_colors = "My Favorite Colors are: "
+
+# Convert the list to a comma separated string using join()
+color_string += ", ".join(colors)
+
+print(fav_colors)
+
+
+
+# 2.) Asks the user to enter their address
+address = input("Enter your address: ")
+address.strip()
+
+# Use the isalnum() function to verify the entered a valid address, replace non alphanumeric characters
+address = address.replace(" ", "")
+address = address.replace(".", "")
+address = address.replace(",", "")
+print("address:", address, address.isalnum())
